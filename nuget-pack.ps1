1241f89984c586650721838dd8df09ed9c1a7624
Dave Glick <dave@somedave.com>
2014-11-10 13:04:40 -0500

    Setting up automatic NuGet AppVeyor deployment.

---
+++ nuget-pack.ps1
@@ -0,0 +1,4 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\FluentBootstrap\FluentBootstrap\bin\Release\FluentBootstrap.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+& $root\FluentBootstrap\.nuget\NuGet.exe pack $root\FluentBootstrap\FluentBootstrap\FluentBootstrap.csproj -Version $versionStr
