af191c49eae7afb91450b9376a30dd0bbf03e5e8
Dave Glick <dave@somedave.com>
2014-11-18 10:23:48 -0500

    Added support for ASP.NET WebPages.

---
+++ FluentBootstrap.WebPages/WebPagesBootstrapHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.WebPages;
+
+namespace FluentBootstrap.WebPages
+{
+    public class WebPagesBootstrapHelper : BootstrapHelper<WebPagesBootstrapHelper>
+    {
+        internal WebPageBase WebPageBase { get; private set; }
+
+        public WebPagesBootstrapHelper(WebPageBase webPageBase)
+        {
+            WebPageBase = webPageBase;
+        }
+
+        protected internal override TextWriter GetWriter()
+        {
+            return WebPageBase.Output;
+        }
+
+        protected internal override object GetItem(object key, object defaultValue)
+        {
+            if (WebPageBase.Context.Items.Contains(key))
+            {
+                return WebPageBase.Context.Items[key];
+            }
+            return defaultValue;
+        }
+
+        protected internal override void AddItem(object key, object value)
+        {
+            WebPageBase.Context.Items[key] = value;
+        }
+    }
+}
