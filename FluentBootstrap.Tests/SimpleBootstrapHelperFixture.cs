31cf2f927216d18ab6f555e7a055370ad294a552
Dave Glick <dave@daveaglick.com>
2015-03-22 20:23:50 -0400

    Added a small test for SimpleBootstrapHelper

---
+++ FluentBootstrap.Tests/SimpleBootstrapHelperFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FluentBootstrap.Tests
+{
+    [TestFixture]
+    public class SimpleBootstrapHelperFixture
+    {
+        [Test]
+        public void SimpleBootstrapHelperProducesCorrectHtmlForInput()
+        {
+            // Given
+            SimpleBootstrapHelper helper = new SimpleBootstrapHelper();
+
+            // When
+            string input = helper.Input("input-name", "My Input").ToString();
+
+            // Then
+            Assert.AreEqual(@"
+<label for=""input-name"" class=""control-label"">My Input</label>
+<input type=""text"" name=""input-name"" class=""form-control"">", input);
+        }
+    }
+}
